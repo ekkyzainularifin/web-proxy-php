2240a7503ce71b49c08c217f1d7f6b88e5f30d12
Ekky Zainul Arifin <ekkyzainularifin@gmail.com>
2021-11-18 14:33:39 +0700

    change template

--- templates/main.php
+++ templates/main.php
@@ -1,90 +1,74 @@
 <!DOCTYPE html>
-<html>
-<head>
-
-<title>PHP-Proxy</title>
-
-<meta name="generator" content="php-proxy.com">
-<meta name="version" content="<?=$version;?>">
-
-<style type="text/css">
-html body {
-	font-family: Arial,Helvetica,sans-serif;
-	font-size: 12px;
-}
-
-#container {
-	width:500px;
-	margin:0 auto;
-	margin-top:150px;
-}
-
-#error {
-	color:red;
-	font-weight:bold;
-}
-
-#frm {
-	padding:10px 15px;
-	background-color:#FFC8C8;
-	
-	border:1px solid #818181;
-	
-	-webkit-border-radius: 8px;
-	-moz-border-radius: 8px;
-	border-radius: 8px;
-}
-
-#footer {
-	text-align:center;
-	font-size:10px;
-	margin-top:35px;
-	clear:both;
-}
-</style>
-
-</head>
-
-<body>
-
-
-<div id="container">
-
-	<div style="text-align:center;">
-		<h1 style="color:blue;">PHP-Proxy</h1>
-	</div>
-	
-	<?php if(isset($error_msg)){ ?>
-	
-	<div id="error">
-		<p><?php echo strip_tags($error_msg); ?></p>
-	</div>
-	
-	<?php } ?>
-	
-	<div id="frm">
-	
-	<!-- I wouldn't touch this part -->
-	
-		<form action="index.php" method="post" style="margin-bottom:0;">
-			<input name="url" type="text" style="width:400px;" autocomplete="off" placeholder="http://" />
-			<input type="submit" value="Go" />
-		</form>
-		
-		<script type="text/javascript">
-			document.getElementsByName("url")[0].focus();
-		</script>
-		
-	<!-- [END] -->
-	
-	</div>
-	
-</div>
-
-<div id="footer">
-	Powered by <a href="//www.php-proxy.com/" target="_blank">PHP-Proxy</a> <?php echo $version; ?>
-</div>
-
-
-</body>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>DiProxy.com</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top">DiProxy</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-primary bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to DiProxy.com</h1>
+                <p class="lead">access everything without a hitch</p>
+                <form action="index.php" method="post">
+                    <div class="form-group px-5">
+                        <div class="input-group mb-3">
+                            <input type="text" name="url" class="form-control form-control-lg" placeholder="https://" aria-label="https://" aria-describedby="basic-addon2">
+                            <div class="input-group-append">
+                                <button class="btn btn-dark btn-lg" type="submit">Go</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+                <?php if(isset($error_msg)){ ?>
+                    <div id="error">
+                        <p><?php echo strip_tags($error_msg); ?></p>
+                    </div>
+                <?php } ?>
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="about">
+            <div class="container px-2">
+                <div class="row gx-2 justify-content-center">
+                    <div class="col-lg-8">
+                        <h2>About this page</h2>
+                        <p class="lead">You can access blocked websites using an encrypted connection.</p>
+                        <ul>
+                            <li>Hide yout IP address.</li>
+                            <li>Makes all websites SSL encrypted.</li>
+                            <li>Can be used on any operating system.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; diproxy.com 2021</p></div>
+        </footer>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="js/scripts.js"></script>
+        <script type="text/javascript">
+            document.getElementsByName("url")[0].focus();
+        </script>
+    </body>
 </html>
